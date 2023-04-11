21ce861c7ca6589996e5bfdcf3984b7870532449
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-11 07:04:24 -0700

    Task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Class defination"""
+
+
 class MyInt(int):
     def __eq__(self, other):
+        """Returns an integer"""
+
+
         return int(self) != int(other)
     
     def __ne__(self, other):
+        """Returns an integer"""
+
+
         return int(self) == int(other)

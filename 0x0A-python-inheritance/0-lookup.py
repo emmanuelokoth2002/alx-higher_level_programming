f9a408049dfe4bbda89b5a40f63a92f8ad29e3bc
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-11 06:07:27 -0700

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-"""Return a list of available attributes and methods of an object."""
-def lookup(obj):    
+"""Function to return a list of available attributes and methods of an object."""
+
+
+def lookup(obj):
+    """look up function"""
+
+
     return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or attr.startswith('__')]

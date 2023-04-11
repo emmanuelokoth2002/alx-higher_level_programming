20dbdc3f6b034b8dca24732942316fd025498a18
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-11 00:07:29 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Base class for geometry calculations."""
+
+
+    def area(self):
+        """Compute area of geometry."""
+
+        raise Exception("area() is not implemented")

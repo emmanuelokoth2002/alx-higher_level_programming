bd9c3d4eaad0dba60bce9bec4a24ba51520b5493
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-10 23:57:51 -0700

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited (directly or indirectly)
+    from a_class; otherwise False."""
+
+
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

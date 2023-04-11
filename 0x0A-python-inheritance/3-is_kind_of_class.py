ab0e41d38da4edad78ebe24128702b681ac3f28e
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-11 06:40:44 -0700

    Task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+"""Function to check object instance"""
+
+
 def is_kind_of_class(obj, a_class):
-    """Return True if obj is an instance of a_class or any of its subclasses; otherwise False."""
+    """Return True if obj is an instance of a_class; otherwise False."""
 
 
     return isinstance(obj, a_class)

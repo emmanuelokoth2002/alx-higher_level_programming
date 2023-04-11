3fc85a4defad8935797fa267ec5115de88f313c4
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-11 00:15:05 -0700

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Base class for geometry calculations."""
+
+    
+    def area(self):
+        """Compute area of geometry."""
+
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """Validate input value as an integer greater than 0."""
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")

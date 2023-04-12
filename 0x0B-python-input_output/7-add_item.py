01d70a07687d61fb5ccc4786e19cd20a121c012b
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-12 11:39:18 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 
-""" script that adds all arguments to a Python list"""
+"""script that adds all arguments to a Python list"""
 
 
+
+import json
 import sys
-from save_to_json_file import 5-save_to_json_file
-from load_from_json_file import 6-load_from_json_file
+from os import path
 
-my_list = []
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if path.isfile('add_item.json'):
+    my_list = load_from_json_file('add_item.json')
+else:
+    my_list = []
 
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
-save_to_json_file(my_list, "add_item.json")
-loaded_list = load_from_json_file("add_item.json")
-print(loaded_list)
+save_to_json_file(my_list, 'add_item.json')

a928d71bdd30d1bb3c563a3d060592fb7870b2fa
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-12 06:22:40 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,11 +4,8 @@
 
 
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-"""Create an empty list to hold the arguments"""
-
+from save_to_json_file import 5-save_to_json_file
+from load_from_json_file import 6-load_from_json_file
 
 my_list = []
 
@@ -16,6 +13,5 @@ for arg in sys.argv[1:]:
     my_list.append(arg)
 
 save_to_json_file(my_list, "add_item.json")
-
 loaded_list = load_from_json_file("add_item.json")
 print(loaded_list)

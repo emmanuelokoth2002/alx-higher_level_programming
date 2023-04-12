cb60cbdd0bfd0a1614eda386eb42ab2714af942b
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-12 11:35:21 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,9 +2,11 @@
 """function that reads a text file"""
 
 
+
 def read_file(filename=""):
     """Open the file with the specified filename in read mode and UTF-8 encoding"""
 
-    with open(filename, mode='r', encoding='utf-8') as file:
-        contents = file.read()
-        print(contents)
+
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")

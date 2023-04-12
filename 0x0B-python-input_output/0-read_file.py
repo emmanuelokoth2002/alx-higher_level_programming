2d5ad6bad7264aafb56b2935178fba337ac2c462
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-12 05:25:47 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,9 +6,5 @@ def read_file(filename=""):
     """Open the file with the specified filename in read mode and UTF-8 encoding"""
 
     with open(filename, mode='r', encoding='utf-8') as file:
-        """Read the contents of the file into a string variable"""
-
         contents = file.read()
-        """Print the contents of the file to stdout (the console)"""
-
         print(contents)

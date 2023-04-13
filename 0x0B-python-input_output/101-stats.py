91c345010bb38acbae3d4b81c2751e9dab696288
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-12 20:02:25 -0700

    Task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 
+import sys
+
 """initialize variables and data structures"""
 
-
-
 total_file_size = 0
-status_codes = defaultdict(int)
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
 try:
     """read input line by line"""
 
-
     for line in sys.stdin:
         parts = line.split()
         ip_address = parts[0]
-        status_code = parts[8]
-        file_size = int(parts[9])    
+        status_code = int(parts[8])
+        file_size = int(parts[9])
         
-
         total_file_size += file_size
         status_codes[status_code] += 1
         line_count += 1
@@ -26,10 +24,13 @@ try:
         if line_count % 10 == 0:
             print(f"Total file size: {total_file_size}")
             for code in sorted(status_codes.keys()):
-                print(f"{code}: {status_codes[code]}")
+                if status_codes[code] > 0:
+                    print(f"{code}: {status_codes[code]}")
             print("---")
             
 except KeyboardInterrupt:
     print(f"Total file size: {total_file_size}")
     for code in sorted(status_codes.keys()):
-        print(f"{code}: {status_codes[code]}")
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
+

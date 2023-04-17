1efb71859407f399b0ebe7b6387c94a233cf3a90
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-17 14:48:33 -0700

    Updates

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
-""" Module that contains the Square class """
+"""Module containing the Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A representation of a square"""
+    """Class that defines a Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square instance"""
+        """Constructor for the Square class"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Return a string representation of the Square instance"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        """Returns the string representation of the Square"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Getter method for the size attribute"""
+        """Getter for size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter method for the size attribute"""
+        """Setter for size"""
         self.width = value
         self.height = value
-
-    def update(self, *args, **kwargs):
-        """Update the attributes of the Square instance"""
-        if args:
-            attrs = ["id", "size", "x", "y"]
-            for attr, value in zip(attrs, args):
-                setattr(self, attr, value)
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """Return a dictionary representation of the Square instance"""
-        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
 

0fef8a5b7247cc46f4dd72d8b044b43bf078df7c
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-16 20:51:51 -0700

    Task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,91 @@
 #!/usr/bin/python3
+import json
 
 """Class defination"""
 
 
 class Base:
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        obj_list = []
+        if list_objs is not None:
+            for obj in list_objs:
+                obj_list.append(obj.to_dictionary())
+        with open(filename, mode="w", encoding="utf-8") as my_file:
+            my_file.write(cls.to_json_string(obj_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        obj_list = []
+        try:
+            with open(filename, mode="r", encoding="utf-8") as my_file:
+                obj_list_dicts = cls.from_json_string(my_file.read())
+                for obj_dict in obj_list_dicts:
+                    obj_list.append(cls.create(**obj_dict))
+        except FileNotFoundError:
+            pass
+        return obj_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode="w", newline="") as my_file:
+            if list_objs is not None:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                elif cls.__name__ == "Square":
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(my_file, fieldnames=fieldnames)
+                writer.writeheader()
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = cls.__name__ + ".csv"
+        obj_list = []
+        try:
+            with open(filename, mode="r", newline="") as my_file:
+                reader = csv.DictReader(my_file)
+                for row in reader:
+                    for key, value in row.items():
+                        row[key] = int(value)
+                    obj_list.append(cls.create(**row))
+        except FileNotFoundError:
+            pass
+        return obj_list
+            

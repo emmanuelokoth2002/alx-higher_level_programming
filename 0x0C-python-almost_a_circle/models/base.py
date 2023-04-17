5ea9b173274a01515e78552215af499f43161777
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-17 15:35:10 -0700

    updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''Module for Base class.'''
 from json import dumps, loads
 import csv
+import turtle
 
 
 
@@ -124,4 +125,35 @@ class Base:
                          "x": row[2], "y": row[3]}
                 ret.append(cls.create(**d))
         return ret
+
+        @staticmethod
+    def draw(list_rectangles, list_squares):
+        screen = turtle.Screen()
+        screen.setup(width=800, height=600)
+        screen.title("Rectangles and Squares")
+
+        pen = turtle.Turtle()
+        pen.speed(0)
+
+        for rect in list_rectangles:
+            pen.penup()
+            pen.goto(rect.x, rect.y)
+            pen.pendown()
+            pen.color("blue")
+            for _ in range(2):
+                pen.forward(rect.width)
+                pen.left(90)
+                pen.forward(rect.height)
+                pen.left(90)
+
+        for square in list_squares:
+            pen.penup()
+            pen.goto(square.x, square.y)
+            pen.pendown()
+            pen.color("red")
+            for _ in range(4):
+                pen.forward(square.size)
+                pen.left(90)
+
+        turtle.done()
 

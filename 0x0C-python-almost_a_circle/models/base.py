b1c4196d4aac6981fed599aa46234b0533221c45
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-17 15:02:59 -0700

    Updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module for Base class."""
-
-
+'''Module for Base class.'''
 from json import dumps, loads
 import csv
+
+
 
 class Base:
     """A representation of the base of our OOP hierarchy."""

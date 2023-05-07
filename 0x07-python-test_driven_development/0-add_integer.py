df683092413836243d82371c591f1f2de4c27bcc
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-07 07:53:45 -0700

    Updates

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,12 +7,12 @@ def add_integer(a, b=98):
 
     """Check if a and b are integers or floats"""
 
-    if not (isinstance(a, int) or isinstance(a, float)):
+    if not isinstance(a, (int,float)):
         raise TypeError("a must be an integer or b must be an integer")
-    if not (isinstance(b, int) or isinstance(b, float)):
+    if not isinstance(b, (int,float)):
         raise TypeError("a must be an integer or b must be an integer")
     """Cast a and b to integers if they are floats"""
 
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
+    a = int(a)
+    b = int(b)
     return a + b

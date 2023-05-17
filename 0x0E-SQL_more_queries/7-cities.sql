5acff891f5b65cb2d6afd2a28627d7de37dfedf0
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-17 12:52:54 -0700

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,7 @@
 --  database hbtn_0d_usa and the table cities
-USE hbtn_0d_usa;
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE cities (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        state_id INT NOT NULL,
-        name VARCHAR(256) NOT NULL,
-        FOREIGN KEY (state_id) REFERENCES states(id)
-    );
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	);

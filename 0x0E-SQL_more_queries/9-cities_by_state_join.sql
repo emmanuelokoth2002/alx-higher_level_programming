c639c817603e92e81802d6e97375ac6f5395bc6e
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-17 12:04:43 -0700

    Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,2 @@
 -- List All Cities
-USE hbtn_0d_usa;
-SELECT cities.id, cities.name, states.name
-FROM @db_name.cities AS cities, @db_name.states AS states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON states.id = cities.state_id;

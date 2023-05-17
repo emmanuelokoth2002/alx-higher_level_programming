f17d97fac56611d227a295a00b6371b95cb05832
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-17 12:50:18 -0700

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,5 @@
 -- script that creates the database hbtn_0d_usa and the table states
-IF @table_exists = 0 THEN
-    CREATE TABLE states (
+CREATE TABLE states (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(256) NOT NULL
     );

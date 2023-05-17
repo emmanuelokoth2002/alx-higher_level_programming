af4d8a8a0ba70459f41747fe669bdad49e76b9dd
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-17 06:51:00 -0700

    Updates

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,14 +1,5 @@
--- Script: Create Table force_name
-SET @database_name = 'your_database_name';
-SELECT COUNT(*) INTO @table_exists
-FROM information_schema.tables
-WHERE table_schema = @database_name
-AND table_name = 'force_name';
-
--- Create the table only if it doesn't already exist
-IF @table_exists = 0 THEN
-    CREATE TABLE @database_name.force_name (
-        id INT,
-        name VARCHAR(256) NOT NULL
-    );
-END IF;
+-- Script: Create Table force_name on your MySQL server.
+CREATE TABLE IF NOT EXISTS @database_name.force_name (
+  id INT,
+  name VARCHAR(256) NOT NULL
+);

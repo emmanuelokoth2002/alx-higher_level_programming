6f13acdef812ae8d5e70a4029108bf7d21438e0b
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-17 06:46:06 -0700

    Updates

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,14 @@
 -- Script: Create Table force_name
 SET @database_name = 'your_database_name';
-CREATE TABLE IF NOT EXISTS @database_name.force_name (
-  id INT,
-  name VARCHAR(256) NOT NULL
-);
+SELECT COUNT(*) INTO @table_exists
+FROM information_schema.tables
+WHERE table_schema = @database_name
+AND table_name = 'force_name';
+
+-- Create the table only if it doesn't already exist
+IF @table_exists = 0 THEN
+    CREATE TABLE @database_name.force_name (
+        id INT,
+        name VARCHAR(256) NOT NULL
+    );
+END IF;

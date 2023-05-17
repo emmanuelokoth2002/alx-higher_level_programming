ab8f3cfc67c79dc3eb2b1f8c6245a76efb6186c2
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-17 08:04:34 -0700

    Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+--  database hbtn_0d_usa and the table cities
+CREATE TABLE cities (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        state_id INT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        FOREIGN KEY (state_id) REFERENCES states(id)
+    );

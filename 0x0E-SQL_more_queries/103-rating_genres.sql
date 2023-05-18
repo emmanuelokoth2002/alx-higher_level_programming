8511bd793802dd8c29b62aa499ae5a6be91bcd4c
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-18 01:16:05 -0700

    Task 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,4 @@
 -- Script: List Genres by Rating
-USE hbtn_0d_tvshows_rate;
 SELECT tv_genres.name, SUM(ratings.rating) AS rating_sum
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id

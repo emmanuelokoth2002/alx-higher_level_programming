d1ea91b72f429b0c2aa957272c9f90bb962488b8
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-18 01:22:58 -0700

    Task 18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,8 @@
 -- Script: List Shows Without Comedy Genre
-USE hbtn_0d_tvshows;
-SET @comedy_genre_id = (
-  SELECT id
-  FROM tv_genres
-  WHERE name = 'Comedy'
-  LIMIT 1
-);
-SELECT tv_shows.title
-FROM tv_shows
-WHERE tv_shows.id NOT IN (
-  SELECT show_id
-  FROM tv_show_genres
-  WHERE genre_id = @comedy_genre_id
-)
-ORDER BY tv_shows.title ASC;
+SELECT s.title FROM tv_shows s WHERE s.title NOT IN (
+      SELECT s.title
+      FROM tv_shows s
+      INNER JOIN tv_show_genres m ON s.id = m.show_id
+      INNER JOIN tv_genres g ON m.genre_id = g.id
+      WHERE g.name = 'Comedy'
+) ORDER BY s.title ASC;

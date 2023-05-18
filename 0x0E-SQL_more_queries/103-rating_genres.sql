baab14d0856f2174fa699e1b1d952e1de274238a
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-18 01:18:19 -0700

    Task 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,6 @@
 -- Script: List Genres by Rating
-SELECT tv_genres.name, SUM(ratings.rating) AS rating_sum
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN ratings ON tv_shows.id = ratings.show_id
-GROUP BY tv_genres.id
-ORDER BY rating_sum DESC;
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY rating DESC;

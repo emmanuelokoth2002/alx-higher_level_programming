3244cb83627aa124dd8ece68e4cd8fccaf6370a4
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-12 02:58:15 -0700

    Updates

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+if (arg1 && arg2) {
+  console.log(`${arg1} is ${arg2}`);
+} else {
+  console.log("Missing arguments");
+}

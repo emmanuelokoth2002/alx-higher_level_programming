a1373c0311b441a7d5870e371239be4a93ea528a
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-12 03:16:03 -0700

    Updates

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+const converted = parseInt(arg);
+
+if (Number.isNaN(converted)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${converted}`);
+}

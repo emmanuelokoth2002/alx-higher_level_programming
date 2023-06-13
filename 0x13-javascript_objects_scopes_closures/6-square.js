3d00be327dba6be944c879a7f14f25980f1821ea
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-13 06:40:55 -0700

    Updates

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;

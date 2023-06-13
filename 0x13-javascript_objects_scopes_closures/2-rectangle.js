023e2be2b22ec39750d04c487ac251d6362e703f
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-13 11:57:46 -0700

    Updates

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return { };
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
     }
-    this.width = w;
-    this.height = h;
   }
 }
 module.exports = Rectangle;

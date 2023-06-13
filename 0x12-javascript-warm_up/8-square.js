92a46e4d30a7db5a86166477989736ae6f3a7d06
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-12 19:23:45 -0700

    Updates

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 if (!isNaN(size)) {
-  for (let x = 0; x < size; i++) {
+  for (let x = 0; x < size; x++) {
     console.log('X'.repeat(size));
   }
 } else { console.log('Missing size'); }

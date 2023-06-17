f6137c9992f236203ac8d216bcbffa47060dc57d
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-16 22:29:54 -0700

    Updates

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" lists all states with a name starting with N
+    (upper N) from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./1-filter_states.py < mysql_username >
+                < mysql_password > < database_name >")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+        cursor = db.cursor()
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+
+        cursor.execute(query)
+
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)

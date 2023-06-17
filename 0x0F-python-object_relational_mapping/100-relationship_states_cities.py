50ffbb33d5949931cd0fb392659f010e8fdc9738
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-17 05:44:40 -0700

    Updates

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Creates the State "California" with the City "San Francisco"
+in the database hbtn_0e_100_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    california = State(name="California")
+    san_francisco = City(name="San Francisco")
+    california.cities.append(san_francisco)
+    session.add(california)
+    session.commit()
+
+    session.close()

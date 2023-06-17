d70dd756b57897008b07d9dd6c7022307fa90f16
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-17 13:03:56 -0700

    Updates

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-"""Lists all states with safe MySQL query"""
+"""Lists all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./safe_states.py [username] [password] [database] [state_name]")
-        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    username = MySQLdb.escape_string(sys.argv[1])
-    password = MySQLdb.escape_string(sys.argv[2])
-    database = MySQLdb.escape_string(sys.argv[3])
-    state_name = MySQLdb.escape_string(sys.argv[4])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
 
-    try:
-        connection = MySQLdb.connect(host="localhost", port=3306,
-                                     user=username, passwd=password, db=database)
-        cursor = connection.cursor()
-        sql = "SELECT * FROM states WHERE name=%s ORDER BY states.id ASC"
-        cursor.execute(sql, (state_name,))
-        query_rows = cursor.fetchall()
-        for row in query_rows:
-            print(row)
-        cursor.close()
-        connection.close()
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()

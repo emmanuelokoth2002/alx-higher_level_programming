c659eb85253c62ba18fbfd1790d838a301034ffc
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-30 12:10:59 -0700

    Updates

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that fetches a URL using the requests
+package and displays the body of the response.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))

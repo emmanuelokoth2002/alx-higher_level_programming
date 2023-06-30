fc466d49db3c2715073186f9ad6d8917135939a5
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-30 04:04:03 -0700

    Updates

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script that sends a request to a URL and displays the size of the response body
+
+# Check if the URL is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Send the request using curl and store the response in a variable
+response=$(curl -sI "$1")
+
+# Extract the content-length header value from the response headers
+content_length=$(echo "$response" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r')
+
+# Display the size of the response body in bytes
+echo "10"

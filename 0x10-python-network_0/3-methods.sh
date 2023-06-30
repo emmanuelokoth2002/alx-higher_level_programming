7673442ae0271664aa3955972f46ef6d1c1e45f0
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-30 06:14:42 -0700

    Updates

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that displays all HTTP methods accepted by a server for a URL
-curl -sI "$1" | grep -i Allow | awk '{print $2}'
+curl -sI "$1" | grep "Allow:" | cut -d ' ' -f2-

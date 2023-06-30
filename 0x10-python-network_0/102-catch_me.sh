138587bfd4f959a88e0539877e72769c24807e64
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-30 06:53:06 -0700

    Updates

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 #script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+

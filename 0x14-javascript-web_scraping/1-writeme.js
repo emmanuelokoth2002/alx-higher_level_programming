4a3e47b1652e0ea6f3e401c076af2a8fe79b7c3b
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-25 17:15:05 +0000

    Updates

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -15,7 +15,5 @@ const contentToWrite = process.argv[3];
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(`Content written to ${filePath}`);
   }
 });

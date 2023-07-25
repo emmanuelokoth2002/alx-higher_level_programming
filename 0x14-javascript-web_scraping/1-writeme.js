efce8db50a959ab0fe7f076f0b0f7ce660758f02
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-25 17:12:36 +0000

    Updates

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if the correct number of command-line arguments is provided
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <content_to_write>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+// Write the string content to the file in utf-8
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Content written to ${filePath}`);
+  }
+});
